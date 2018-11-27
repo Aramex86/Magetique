26587f91c0d827894d3bfb2ccd74a1262b453e7e
Aramex <slavbez@mail.ru>
2018-11-27 14:04:12 +0200

    animation start

--- js/script.js
+++ js/script.js
@@ -1,10 +1,31 @@
 $(function () {
-    $('.inside-hover').mouseover(function () {
-        $('.inside-skale').show();
+    $(window).scroll(function () {
+        var height = $(window).scrollTop();
+        if(height > 150){
+            $('.arrow-top').fadeIn(200);
+        }else {
+            $('.arrow-top').fadeOut(200);
+        }
     });
-    $('.inside-hover').mouseout(function () {
-        $('.inside-skale').hide();
+    $('.arrow-top').click(function (event) {
+        event.preventDefault();
+        $('html, body').animate({scrollTop:0},'slow');
+        return false
     });
+    $(window).scroll(function () {
+        var headTop = $(window).scrollTop();
+        if (headTop > 50){
+            $('.logo').hide();
+            $('.navi-bar ul').css('width','100%');
+        }else {
+            $('.logo').show();
+            $('.navi-bar ul').css('width','75%');
+        }
+
+    });
+        
+
+
 
 
 
