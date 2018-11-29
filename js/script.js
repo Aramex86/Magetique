495b81328d2efb4681b6b809256650d8b6b132df
Aramex <slavbez@mail.ru>
2018-11-29 14:28:44 +0200

    animation start 2

--- js/script.js
+++ js/script.js
@@ -3,8 +3,15 @@ $(function () {
         var height = $(window).scrollTop();
         if(height > 150){
             $('.arrow-top').fadeIn(200);
+            $('.logo').fadeOut(100);
+            $('.logo').css('padding-left','0px');
+            $('.navi-bar ul').css('width','100%');
+            $('.navi-bar ul').css('padding-top','0px');
         }else {
             $('.arrow-top').fadeOut(200);
+            $('.logo').fadeIn(100);
+            $('.logo').css('padding-left','119px');
+            $('.navi-bar ul').css('width','75%');
         }
     });
     $('.arrow-top').click(function (event) {
@@ -12,17 +19,58 @@ $(function () {
         $('html, body').animate({scrollTop:0},'slow');
         return false
     });
-    $(window).scroll(function () {
-        var headTop = $(window).scrollTop();
-        if (headTop > 50){
-            $('.logo').hide();
-            $('.navi-bar ul').css('width','100%');
-        }else {
-            $('.logo').show();
-            $('.navi-bar ul').css('width','75%');
-        }
 
+    var firstBlock = $('.img1');
+    var pos = firstBlock.position();
+    $(window).scroll(function () {
+        var windowpos = $(window).scrollTop();
+        if (windowpos >= pos.top & windowpos <= 1000){
+            $('.img1').addClass('animated fadeInRight');
+            $('.img2').addClass('animated fadeInRight');
+            $('.img4').addClass('animated fadeInLeft');
+            $('.img5').addClass('animated fadeInLeft');
+        }
     });
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+    function myFunction() {
+       // if (document.body.scrollTop > 633 || document.documentElement.scrollTop > 633)
+        if (document.body.scrollTop === 633 || document.documentElement.scrollTop > 633) {
+            $('.img1').addClass('animated fadeInRight');
+            $('.img2').addClass('animated fadeInRight');
+            $('.img4').addClass('animated fadeInLeft');
+            $('.img5').addClass('animated fadeInLeft');
+        } else {
+            $('.img1').removeClass('animated fadeInRight');
+            $('.img2').removeClass('animated fadeInRight');
+            $('.img4').removeClass('animated fadeInLeft');
+            $('.img5').removeClass('animated fadeInLeft');
+        }
+    }
+
+*/
+
+
+
+
+
+
+
+
+
+
+
         
 
 
